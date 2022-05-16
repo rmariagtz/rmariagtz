7b0fdc5c6befe3149ae205551d736c0d63a9832d
rmariagtz <105613649+rmariagtz@users.noreply.github.com>
2022-05-16 16:50:13 -0500

    Update spades.sh

--- spades.sh
+++ spades.sh
@@ -14,7 +14,6 @@ for file1 in out*1*.fastq
 do	
 SAMPLE_NAME=$(basename -s "_R1.fastq" ${file1})
 echo "running $SAMPLE_NAME"
-done
 nohup spades.py -o ${SAMPLE_NAME}_SPADES --meta -1 ${SAMPLE_NAME}_R1.fastq -2 ${SAMPLE_NAME}_R2.fastq  -t 10 -k 21,33,55,77,99,127 &
 
 	
