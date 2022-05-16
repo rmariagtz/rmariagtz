d3a13809c2b7e81d0f5e4d60a661a6fee3934566
rmariagtz <105613649+rmariagtz@users.noreply.github.com>
2022-05-15 19:09:54 -0500

    Add files via upload
    Runnig spades in a regular server

---
+++ spades.sh
@@ -0,0 +1,21 @@
+set -e
+set -u
+set -o pipefail
+
+#AUTHOR ROSA MARIA GUTIERREZ
+#RUN SPADES METAGENOMES PAIR END
+#input: left and right cleaned reads
+
+
+#my input files path
+cd /hd1/rmaria/FASTQ/
+
+for file1 in out*1*.fastq
+do	
+SAMPLE_NAME=$(basename -s "_R1.fastq" ${file1})
+echo "running $SAMPLE_NAME"
+done
+nohup spades.py -o ${SAMPLE_NAME}_SPADES --meta -1 ${SAMPLE_NAME}_R1.fastq -2 ${SAMPLE_NAME}_R2.fastq  -t 10 -k 21,33,55,77,99,127 &
+
+	
+done
